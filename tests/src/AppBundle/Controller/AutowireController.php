3d5c189f0916ce6706c71242dc1a001e79e3eb91
mael.sourisseau <mael.sourisseau@conserto.pro>
2024-12-13 10:50:01 +0100

    Fix tests

--- tests/src/AppBundle/Controller/AutowireController.php
+++ tests/src/AppBundle/Controller/AutowireController.php
@@ -4,30 +4,36 @@
  *
  */
 namespace AppBundle\Controller;
+
 use AppBundle\Model\MyDb1\PublicSchema\ConfigModel;
+use PommProject\Foundation\Exception\FoundationException;
 use PommProject\Foundation\Pomm;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 /**
  * @author Mikael Paris <stood86@gmail.com>
  */
 class AutowireController
 {
-    public function getAutowireAction(
-        string $name,
-        Pomm $pomm,
-        Environment $engine
-    ): Response
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws FoundationException
+     * @throws LoaderError
+     */
+    public function getAutowireAction(string $name, Pomm $pomm, Environment $engine): Response
     {
         $config = $pomm
             ->getDefaultSession()
             ->getModel(ConfigModel::class)
             ->findByPk(['name' => $name]);
 
-        return new Response($engine->render(
-            'Front/get.html.twig',
-            compact('config')
-        ));
+        return new Response(
+            $engine->render('Front/get.html.twig', compact('config'))
+        );
     }
 }
